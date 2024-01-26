39cb5f4afa32528e5d242397b6aa135b43ffdfd0
NFLD99 <NFLD99@users.noreply.github.com>
2024-01-26 18:32:15 -0330

    Create vrcBTN.js

---
+++ vrcBTN.js
@@ -0,0 +1,60 @@
+// ==UserScript==
+// @name         Copy URL
+// @namespace    http://tampermonkey.net/
+// @version      2024-01-26
+// @description  try to take over the world!
+// @author       You
+// @match        https://www.youtube.com/watch?v=*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=youtube.com
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    function addButton() {
+        var currentUrl = window.location.href;
+        var indexOfAmpersand = currentUrl.indexOf('&');
+        var modifiedUrl = indexOfAmpersand !== -1 ? currentUrl.substring(0, indexOfAmpersand) : currentUrl;
+        var ownerElement = document.querySelector("#owner");
+        if (ownerElement && !document.getElementById('copyButton')) {
+            var button = document.createElement('button');
+            button.id = 'copyButton';
+            button.innerText = 'VRC';
+            button.style.background = '#272728';
+            button.style.color = '#f1f1f1';
+            button.style.borderRadius = '50px';
+            button.classList.add('glow-on-hover');
+            button.addEventListener('click', function() {
+                var tempInput = document.createElement('input');
+                tempInput.value = modifiedUrl;
+                document.body.appendChild(tempInput);
+                tempInput.select();
+                document.execCommand('copy');
+                document.body.removeChild(tempInput);
+            });
+            ownerElement.parentNode.insertBefore(button, ownerElement.nextSibling);
+        } else {
+            console.error('Element with id "owner" not found or button already added.');
+        }
+    }
+    var ownerElement = document.querySelector("#owner");
+    if (ownerElement) {
+        addButton();
+    } else {
+        var observer = new MutationObserver(function(mutations) {
+            mutations.forEach(function(mutation) {
+                if (mutation.addedNodes && mutation.addedNodes.length > 0) {
+                    var ownerElement = document.querySelector("#owner");
+                    if (ownerElement) {
+                        observer.disconnect();
+                        addButton();
+                    }
+                }
+            });
+        });
+        observer.observe(document.body, {
+            childList: true,
+            subtree: true
+        });
+    }
+})();
