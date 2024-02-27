3c2b0f0daf173555bc49a29c7beabdf434a56311
NFLD99 <NFLD99@users.noreply.github.com>
2024-02-26 20:55:39 -0330

    Update vrcBTN.js

--- vrcBTN.js
+++ vrcBTN.js
@@ -4,17 +4,15 @@
 // @version      2024-01-26
 // @description  try to take over the world!
 // @author       You
-// @match        https://www.youtube.com/watch?v=*
+// @match        https://www.youtube.com/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=youtube.com
 // @grant        none
 // ==/UserScript==
 
 (function() {
     'use strict';
+
     function addButton() {
-        var currentUrl = window.location.href;
-        var indexOfAmpersand = currentUrl.indexOf('&');
-        var modifiedUrl = indexOfAmpersand !== -1 ? currentUrl.substring(0, indexOfAmpersand) : currentUrl;
         var ownerElement = document.querySelector("#owner");
         if (ownerElement && !document.getElementById('copyButton')) {
             var button = document.createElement('button');
@@ -25,6 +23,9 @@
             button.style.borderRadius = '50px';
             button.classList.add('glow-on-hover');
             button.addEventListener('click', function() {
+                var currentUrl = window.location.href;
+                var indexOfAmpersand = currentUrl.indexOf('&');
+                var modifiedUrl = indexOfAmpersand !== -1 ? currentUrl.substring(0, indexOfAmpersand) : currentUrl;
                 var tempInput = document.createElement('input');
                 tempInput.value = modifiedUrl;
                 document.body.appendChild(tempInput);
@@ -37,16 +38,14 @@
             console.error('Element with id "owner" not found or button already added.');
         }
     }
-    var ownerElement = document.querySelector("#owner");
-    if (ownerElement) {
+
+    function addScript() {
         addButton();
-    } else {
         var observer = new MutationObserver(function(mutations) {
             mutations.forEach(function(mutation) {
                 if (mutation.addedNodes && mutation.addedNodes.length > 0) {
                     var ownerElement = document.querySelector("#owner");
                     if (ownerElement) {
-                        observer.disconnect();
                         addButton();
                     }
                 }
@@ -57,4 +56,14 @@
             subtree: true
         });
     }
+
+    addScript();
+
+    // Listen for DOM changes
+    document.addEventListener("DOMContentLoaded", addScript);
+    window.addEventListener("load", addScript);
+    window.addEventListener("popstate", addScript);
+
+    // Listen for URL changes
+    window.addEventListener("hashchange", addScript);
 })();
