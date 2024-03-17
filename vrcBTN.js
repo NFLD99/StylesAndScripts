1992507c9b4be68d30bc93a43546627ad247332d
NFLD99 <NFLD99@users.noreply.github.com>
2024-03-17 17:16:49 -0230

    Update vrcBTN.js

--- vrcBTN.js
+++ vrcBTN.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Copy URL
 // @namespace    http://tampermonkey.net/
-// @version      5
+// @version      6
 // @description  try to take over the world!
 // @author       You
 // @match        https://www.youtube.com/*
@@ -13,33 +13,6 @@
 (function () {
     'use strict';
 
-    function addButton() {
-        var ownerElement = document.querySelector("#owner");
-        if (ownerElement && !document.getElementById('copyButton')) {
-            checkBlacklist()
-            var button = document.createElement('button');
-            button.id = 'copyButton';
-            button.innerText = 'VRC';
-            button.style.background = '#272728';
-            button.style.color = '#f1f1f1';
-            button.style.borderRadius = '50px';
-            button.classList.add('glow-on-hover');
-            button.addEventListener('click', function () {
-                var currentUrl = window.location.href;
-                var indexOfAmpersand = currentUrl.indexOf('&');
-                var modifiedUrl = indexOfAmpersand !== -1 ? currentUrl.substring(0, indexOfAmpersand) : currentUrl;
-                var tempInput = document.createElement('input');
-                tempInput.value = modifiedUrl;
-                document.body.appendChild(tempInput);
-                tempInput.select();
-                document.execCommand('copy');
-                document.body.removeChild(tempInput);
-            });
-            ownerElement.parentNode.insertBefore(button, ownerElement.nextSibling);
-        } else {
-            console.error('Element with id "owner" not found or button already added.');
-        }
-    }
     function isInBlacklist(videoId, blacklist) {
         return blacklist.includes(videoId);
     }
@@ -76,6 +49,34 @@
                 console.error("Error fetching blacklist:", error);
             }
         });
+    }
+    function addButton() {
+        var ownerElement = document.querySelector("#owner");
+        if (ownerElement && !document.getElementById('copyButton')) {
+            checkBlacklist()
+            var button = document.createElement('button');
+            button.id = 'copyButton';
+            button.innerText = 'VRC';
+            button.style.background = '#272728';
+            button.style.color = '#f1f1f1';
+            button.style.borderRadius = '50px';
+            button.classList.add('glow-on-hover');
+            button.addEventListener('click', function () {
+                var currentUrl = window.location.href;
+                var indexOfAmpersand = currentUrl.indexOf('&');
+                var modifiedUrl = indexOfAmpersand !== -1 ? currentUrl.substring(0, indexOfAmpersand) : currentUrl;
+                var tempInput = document.createElement('input');
+                tempInput.value = modifiedUrl;
+                document.body.appendChild(tempInput);
+                tempInput.select();
+                document.execCommand('copy');
+                document.body.removeChild(tempInput);
+            });
+            ownerElement.parentNode.insertBefore(button, ownerElement.nextSibling);
+        } else {
+            console.log('Element with id "owner" not found or button already added.');
+            checkBlacklist()
+        }
     }
     function addScript() {
         addButton();
